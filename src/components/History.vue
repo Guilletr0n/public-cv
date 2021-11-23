2bed30555f7df405a117c40b2b2824de2792c01e
Guillermo Gonzalez <gonzalezdecastro.guillermo@gmail.com>
2021-11-23 11:33:19 +0100

    first commit

---
+++ src/components/History.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import companies from "./companies.json";
+
+const word = (<any>companies).name;
+const role = (<any>companies).role
+
+</script>
+<template>
+  <div id="history">
+    <ul>
+      <li v-for="company in companies" :key="company.id">
+        <h5>{{company.name}}</h5>
+        <p>{{company.role.en}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
